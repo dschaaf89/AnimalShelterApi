714521b0047bb40daba60434a1221eecea98d5dd
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-10-30 08:42:23 -0700

    added the models for Animal and AnimalShelterContext.

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -1,7 +1,13 @@
 namespace AnimalShelterApi.Models
 {
-    public class Animals
+    public class Animal
     {
-        
+        public string AnimalType { get; set; }
+        public string Breed { get; set; }
+        public string AnimalName { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+        public string Details { get; set; }
+
     }
 }
--- AnimalShelterApi/Models/AnimalShelterContext.cs
+++ AnimalShelterApi/Models/AnimalShelterContext.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 namespace AnimalShelterApi.Models
 {
-    public class AnimalShelterContext
+    public class AnimalShelterContext :DbContext
     {
-        
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+            {
+
+            }
+            public DbSet<Animal> Animals { get; set; }
+            protected override void OnModelCreating(ModelBuilder builder)
+            {
+                
+            }
     }
 }
