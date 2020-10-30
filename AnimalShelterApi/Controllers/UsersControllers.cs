24f58aa33b91a07b27b59a58090fb04e9fc03027
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-10-30 11:57:30 -0700

    added all the UsersController code to make it so you need to authenticate to use API

--- AnimalShelterApi/Controllers/UsersControllers.cs
+++ AnimalShelterApi/Controllers/UsersControllers.cs
@@ -1,7 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using AnimalShelterApi.Services;
+using AnimalShelterApi.Models;
 namespace AnimalShelterApi.Controllers
 {
-    public class UsersControllers
+  [Authorize]
+  [ApiController]
+  [Route("api/[controller]")]
+  public class UsersControllers : ControllerBase
+  {
+    private IUserService _userService;
+    public UsersControllers(IUserService userService)
     {
-        
+      _userService = userService;
     }
+    [AllowAnonymous]
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate([FromBody] User userparam)
+    {
+      var user = _userService.Authenticate(userparam.Username, userparam.Password);
+      if (user == null)
+        return BadRequest(new { message = "Username or password is incorrect" });
+      return Ok(user);
+    }
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+      var users = _userService.GetAll();
+      return Ok(users);
+    }
+  }
 }
