93c331cc4677083dcf576311c368f9eea1a18f4d
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-10-30 16:18:16 -0700

    added validations to the Animal.cs model

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -1,15 +1,22 @@
+using System.ComponentModel.DataAnnotations;
 namespace AnimalShelterApi.Models
 {
-    public class Animal
-    {
-        public int AnimalId { get; set; }
-        public string AnimalType { get; set; }
-        public string Breed { get; set; }
-        public string AnimalName { get; set; }
-        public int Age { get; set; }
-        public string Sex { get; set; }
-        public string Details { get; set; }
-        
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    [Required]
+    public string AnimalType { get; set; }
+    [Required]
+    public string Breed { get; set; }
+    [Required]
+    public string AnimalName { get; set; }
+    [Range(0, 100, ErrorMessage = "Age must be between 0 and 100 beacuse we might have a parrot which live for ever")]
+    public int Age { get; set; }
+    [Required]
+    public string Sex { get; set; }
+    [Required]
+    public string Details { get; set; }
 
-    }
+
+  }
 }
