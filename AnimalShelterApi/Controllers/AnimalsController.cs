768cb5dbfa1a53ace95c1fb426f4e00b471a6b16
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-10-30 09:10:12 -0700

    added the 4 basic routes to the Animal controller to get the animal db, add to , make changes to animal and delete from

---
+++ AnimalShelterApi/Controllers/AnimalsController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using AnimalShelterApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AnimalShelterApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+
+    // GET api/animals
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get()
+    {
+      return _db.Animals.ToList();
+    }
+
+    // GET api/animals/5
+    [HttpGet("{id}")]
+    public ActionResult<Animal> Get(int id)
+    {
+      return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+    }
+
+    // POST api/animals
+    [HttpPost]
+    public void Post([FromBody] Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+    }
+
+    // PUT api/animals/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Animal animal)
+    {
+      animal.AnimalId = id;
+      _db.Entry(animal).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/animals/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+      _db.Animals.Remove(animalToDelete);
+      _db.SaveChanges();
+    }
+  }
+}
--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -2,12 +2,14 @@ namespace AnimalShelterApi.Models
 {
     public class Animal
     {
+        public int AnimalId { get; set; }
         public string AnimalType { get; set; }
         public string Breed { get; set; }
         public string AnimalName { get; set; }
         public int Age { get; set; }
         public string Sex { get; set; }
         public string Details { get; set; }
+        
 
     }
 }
